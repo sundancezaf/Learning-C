7b737701af0b429697474bf35388767f02123007
Zaffy <acehighzilk@gmail.com>
2019-03-26 12:52:15 -0500

    need

---
+++ sumprototype.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int addNumbers(int a, intb); //function prototype
+
+int main(void)
+
+{
+	int n1, n2, sum;
+
+	printf("Enter 2 numbers: ");
+	scanf("%d %d", &n1, &n2);
+
+	sum = addNumbers(n1, n2); //function call
+
+	printf("sum = %d", sum);
+
+	return 0;
+
+}
+
+int addNumbers (int a, int b) //function definition
+	{
+		int result;
+		result = a + b;
+		return result;       //return statement
+	}
