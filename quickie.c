7bde141028178802cc21c1f2e7627d1eaec80829
Zaffy <acehighzilk@gmail.com>
2019-03-26 19:38:08 -0500

    jesus

---
+++ quickie.c
@@ -0,0 +1,42 @@
+
+#include <stdio.h>
+
+int min(int x, int y);
+
+int main (void)
+{
+
+
+int list[3] ={6,4,5};
+int i;
+
+int final = list[0];
+
+	for (i = 0; i<3; i++)
+	{
+		final = min(final, list[i+1]);
+	}
+	printf("%d",final);
+
+	return 0;
+
+}
+
+
+int min (int x, int y)
+
+{
+	int result;
+
+	if (x < y)
+	{
+		result = x;
+	}
+
+	else
+	{
+		result = y;
+	}
+
+	return result;
+}
--- smallestinteger.c
+++
@@ -1,8 +0,0 @@
-/*Find the smallest integer in a list of numbers using a function called min().
-Function needs to take two integers as input and returns the smallest of the two.
-This function will use an if statement with a condition that contains either "less than" or "greater than."
-
-Then use the function to go through an entire list */
-
-
-#include <stdio.h>
