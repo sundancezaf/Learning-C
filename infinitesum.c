be3271e70ad4bf3622341fd95d9bf0fed802f3d1
ZaffyT <acehighzilk@gmail.com>
2019-06-05 17:02:52 -0600

    Infinite sum with user input

---
+++ infinitesum.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main (void)
+
+{
+
+   int number1;
+   int number2;
+   int sum;
+   int i;
+ 
+
+   scanf("%d\n", &number1);
+   scanf("%d", &number2);
+   sum = number1 + number2;
+   printf("%d\n", sum);
+
+   for (i=0; i = 4; i=i+1)
+    {
+       number1 = sum;
+       scanf ("%d", &number2);
+       sum = number1 + number2;
+       printf("%d\n", sum);
+   }
+
+    
+
+   
+
+
+return 0;
+
+
+
+}
