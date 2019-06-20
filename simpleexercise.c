7b7dbf0f1cba193f92fecf767ead0cc16eb00f0c
ZaffyT <acehighzilk@gmail.com>
2019-06-20 18:40:16 -0500

    Ad lib program

---
+++ simpleexercise.c
@@ -0,0 +1,18 @@
+/*This program is a small adlibs game. 
+The user is asked for a word (verb, noun, etc.) and then it prints out the story.*/
+
+#include <stdio.h>
+
+int main (void)
+{
+    printf("Please enter a verb\n");
+    printf("Please enter a noun\n");
+
+
+
+
+
+
+
+return 0;
+}
