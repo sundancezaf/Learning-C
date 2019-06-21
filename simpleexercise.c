3b7da800b6c1385f5749850ed84988eb68661d4d
ZaffyT <acehighzilk@gmail.com>
2019-06-21 01:33:19 -0600

    Mad libs exercise

--- simpleexercise.c
+++ simpleexercise.c
@@ -1,11 +1,33 @@
 /*This program is a small adlibs game. 
 The user is asked for a word (verb, noun, etc.) and then it prints out the story.*/
-
 #include <stdio.h>
 
 int main (void)
 {
-    printf("Please enter a verb\n");
+    char noun1[20];
+    char noun2[20];
+    char noun3[20];
+
+    char verb1[20];
+    char verb2[20];
+    char verb4[20];
+
+    char adverb1[20];
+    char adverb2[20];
+    char adverb3[20];
+    char adverb4[20];
+    
+    char adjective1[20];
+    char adjective2[20];
+    char adjective4[20];
+
+
+
+
+
+
+    printf("Please enter a verb (verb) \n");
+    scanf("%c",&noun);
     printf("Please enter a noun\n");
 
 
