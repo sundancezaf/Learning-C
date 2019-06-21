7f981332d828011e7682cb5b12ea98a1a1bbe079
ZaffyT <acehighzilk@gmail.com>
2019-06-20 20:09:08 -0500

    Ad lib program

--- simpleexercise.c
+++ simpleexercise.c
@@ -4,8 +4,18 @@ The user is asked for a word (verb, noun, etc.) and then it prints out the story
 #include <stdio.h>
 
 int main (void)
+
+
 {
+    char verb1 [20];
+    char noun1[20];
+    char adverb1 [20];
+    char pronoun[20];
+    
     printf("Please enter a verb\n");
+    scanf("")
+
+
     printf("Please enter a noun\n");
 
 
