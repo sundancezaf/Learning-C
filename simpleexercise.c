239f2b3ec55790c5fb61461f506b66da24436c7c
ZaffyT <acehighzilk@gmail.com>
2019-06-22 17:27:04 -0600

    Mad libs exercise

--- simpleexercise.c
+++ simpleexercise.c
@@ -1,54 +1,44 @@
 /*This program is a small adlibs game. 
 The user is asked for a word (verb, noun, etc.) and then it prints out the story.*/
 #include <stdio.h>
-
 int main (void)
-
-
 {
-<<<<<<< HEAD
     char noun1[20];
     char noun2[20];
     char noun3[20];
 
     char verb1[20];
     char verb2[20];
-    char verb4[20];
+    char verb3[20];
 
     char adverb1[20];
     char adverb2[20];
     char adverb3[20];
-    char adverb4[20];
     
     char adjective1[20];
     char adjective2[20];
-    char adjective4[20];
+    char adjective3[20];
 
 
-
-
-
-
-    printf("Please enter a verb (verb) \n");
-    scanf("%c",&noun);
-=======
-    char verb1 [20];
-    char noun1[20];
-    char adverb1 [20];
-    char pronoun[20];
+    printf("Please enter a verb (a verb is an action, state, or occurrence. Ex: run, eats, ate)\n");
+    scanf("%c",&verb1);
     
-    printf("Please enter a verb\n");
-    scanf("")
+    printf("Please enter a profession\n");
+    scanf("%c\n",&noun1);
 
+    printf("Please enter an adjective (an adjective names an attribte. Ex:blue, sad, happy, clean)\n");
+    scanf("%c\n",&adjective1);
 
->>>>>>> 7f981332d828011e7682cb5b12ea98a1a1bbe079
-    printf("Please enter a noun\n");
+    printf("Please enter a color\n");
+    scanf("%c\n",&adjective2);
 
+    printf("Please enter the name of an animal\n");
+    scanf("%c\n",&noun2); 
 
+    printf("Please enter the name of a city\n");
+    scanf("%c\n",&noun3);
 
-
-
-
+    printf("Today I went to the zoo. I %c as fast as I could to escape the %c. \n Next thing I knew I was wearing a %c shirt over my ankles! The %c then\n proceeded to jump on the %c! What a wild night at the %c zoo.", verb1, noun2, adjective2, noun2, noun1, noun3);
 
 return 0;
 }
