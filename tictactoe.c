0985ad3a8f0603a0d92e9a58e68dfca27670fc02
ZaffyT <acehighzilk@gmail.com>
2019-08-17 15:10:12 -0600

    first draft

---
+++ tictactoe.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main (void)
+
+{
+//Have to make the board first
+char board[9];
+char board [2] = "X";
+
+printf("  %c  |", board[2]);
+printf("    |\n");
+printf("____|");
+printf("____|");
+printf("____\n");
+printf("    |");
+printf("    |\n");
+printf("____|");
+printf("____|");
+printf("____\n");
+printf("    |");
+printf("    |");
+
+
+
+
+
+
+return 0;
+
+}
+
