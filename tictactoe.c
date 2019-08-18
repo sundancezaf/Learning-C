0c5c8718540ad6d75df436bee4e6e11a6afe012a
ZaffyT <acehighzilk@gmail.com>
2019-08-18 08:55:38 -0600

    we have a board

--- tictactoe.c
+++ tictactoe.c
@@ -1,27 +1,35 @@
 #include <stdio.h>
+//This function will print the board so we don't have to keep writing the loops
+void printboard(char aboard[5][10] );
 
 int main (void)
 
 {
 //Have to make the board first
-char board[9];
-char board [2] = "X";
 
-printf("  %c  |", board[2]);
-printf("    |\n");
-printf("____|");
-printf("____|");
-printf("____\n");
-printf("    |");
-printf("    |\n");
-printf("____|");
-printf("____|");
-printf("____\n");
-printf("    |");
-printf("    |");
+
+char board[5][10] = {{'\0','\0','\0','|','\0','\0','\0','|','\0','\0'},
+                    {'_','_','_','|','_','_','_','|','_','_'},
+                    {'\0','\0','\0','|','\0','\0','\0','|','\0','\0'},
+                    {'_','_','_','|','_','_','_','|','_','_'},
+                    {'\0','\0','\0','|','\0','\0','\0','|','\0','\0'},
+
+                };
 
 
 
+printboard(board);
+
+//Now we need our players
+char player1= 'X';
+char player2='O';
+int quadrant;
+
+printf("\n");
+printf("\n");
+
+printf("Please enter a quadrant:");
+scanf("%d",&quadrant);
 
 
 
@@ -29,3 +37,17 @@ return 0;
 
 }
 
+
+//This function just prints the board that is being used
+void printboard(char aboard[5][10])
+{
+ int l, k;
+for (l = 0;l<5;l++)
+{
+    printf("\n");
+    for(k=0;k<10;k++)
+    {
+        printf("%c",aboard[l][k]);
+    }
+}
+}
